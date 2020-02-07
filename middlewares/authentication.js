a3793e15be5edc152b93d4cefd15588b3c5aaaec
hikmanisyariful <syarifulfajar017@gmail.com>
2020-02-07 10:17:59 +0700

    add authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,22 @@
+const { verify } = require('../helpers/jwt')
+const { User } = require('../models')
+
+module.exports = function(req, res, next) {
+    try {
+        let decoded = verify(req.headers.token)
+        req.currentUserId = decoded.id
+        User.findByPk(req.currentUserId)
+            .then(function(user) {
+                if (user) {
+                    next()
+                } else {
+                    next({
+                        msg : "not found"
+                    })
+                }
+            })
+            .catch(next)
+    } catch (err) {
+        next(err)
+    }
+}
